b38ccdcd344a29c6c7d82e510fa597e506b627c8
russell258 <duckie258@gmail.com>
2023-05-15 11:40:56 +0800

    first ssf intro to springboot

---
+++ src/main/java/sg/nus/iss/visa/ssf/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package sg.nus.iss.visa.ssf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("This is main method");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/iss/visa/ssf/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.visa.ssf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
